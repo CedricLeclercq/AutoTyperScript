3efc7d6259a98326ecac17ca7f2e1d816eb257c1
Cédric Leclercq <73951315+CedricLeclercq@users.noreply.github.com>
2021-06-02 21:27:28 +0200

    Add files via upload

---
+++ AutomaticTyperMain.py
@@ -0,0 +1,25 @@
+#
+#   Automatic typer
+#   Made by Cédric Leclercq
+#
+#	Make sure that:
+#		- Python has permission to type on your computer
+#		- You have the library 'pyautogui' installed (if not, a simple google search will help you out)
+#
+
+import pyautogui
+import time
+
+# Getting input
+iterations = int(input("Enter the amount of times the script should run: "))
+input = input("Please enter the text you would like to print: ")
+
+# Sleeping to give the user time
+print("Starting in 10 seconds. Go to the location you want the script to type for you.")
+time.sleep(10)
+
+# Looping over the range of iterations, typewriting the data and sleeping for 0.5 seconds
+for i in range(0, iterations):
+    for t in input.split():
+        pyautogui.typewrite(t + '\n')
+        time.sleep(0.5)
